215f8485da72414921244149bda18a6dd85e1af2
“Colin <muriithicolin98@gmail.com>
2023-09-23 06:14:57 +0300

    Updates

---
+++ memoryGame.js
@@ -0,0 +1,33 @@
+//declare img Array
+const images = [
+    {
+        name: 
+
+    }
+    {
+        name: image2
+        
+    }
+    {
+        name: image3
+        
+    }
+]
+
+//create Grid
+function imageGrid (){
+
+
+}
+
+//flip your card function
+function flipCard(){
+
+
+}
+
+//find match function
+function findMatch(){
+
+    
+}
