c0868f0afdb55fcad048ca8f00260586237a0b9d
“Colin <muriithicolin98@gmail.com>
2023-09-24 16:00:34 +0300

    Updates

--- memoryGame.js
+++ memoryGame.js
@@ -1,27 +1,75 @@
-//declare img Array
-const images = [
-    {
-        name: 
+document.addEventListener('DOMContentLoaded', ()=> {
 
-    }
+//declare img Array
+const cardArray = [
     {
-        name: image2
-        
-    }
+        name: 'another',
+        img: 'Images/another.png'
+    },
     {
-        name: image3
-        
+        name: 'another',
+        img: 'Images/another.png'
+    },
+    {
+        name: 'baptism',
+        img: 'Images/baptism.png'
+    },
+    {
+        name: 'baptism',
+        img: 'Images/baptism.png'
+    },
+    {
+        name: 'chakras',
+        img: 'Images/chakras.png'
+    },
+    {
+        name: 'chakras',
+        img: 'Images/chakras.png'
+    },
+    {
+        name: 'czinger',
+        img: 'Images/czinger.png'
+    },
+    {
+        name: 'czinger',
+        img: 'Images/czinger.png'
+    },
+    {
+        name: 'tartaria',
+        img: 'Images/tartaria.png'
+    },
+    {
+        name: 'tartaria',
+        img: 'Images/tartaria.png'
+    },
+    {
+        name: 'spaceship',
+        img: 'Images/spaceship.png'
+    },
+    {
+        name: 'spaceship',
+        img: 'Images/spaceship.png'
     }
 ]
 
+const grid = document.querySelector('.grid')
+
+
 //create Grid
-function imageGrid (){
-
-
+function createBoard(){
+for (let i = 0; i < cardArray.length; i++){
+    const card = document.createElement('img');
+    card.setAttribute('src', 'Images/blue.png')
+    card.setAttribute('data-id', i);
+    //card.addEventListener('click', flipCard);
+    grid.appendChild(card);
+    }
 }
+createBoard()
 
 //flip your card function
 function flipCard(){
+
 
 
 }
@@ -31,3 +79,4 @@ function findMatch(){
 
     
 }
+})
